6e18fc1159034997b2cdd393f5a181b5871db3fc
ksn38 <ksn38@yandex.ru>
2020-10-08 21:25:49 +0300

    -

--- blog/settings.py
+++ blog/settings.py
@@ -137,6 +137,7 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+import os
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
