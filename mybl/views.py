88a24efc947580c7b61a65a76506ea7afd68d2fb
ksn38 <ksn38@yandex.ru>
2020-10-08 21:11:16 +0300

    -

---
+++ mybl/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from mybl.models import Topic, Entry
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from mybl.forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, 'mybl/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'mybl/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'mybl/topic.html', context)
+
+@login_required
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            new_topic = form.save(commi=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('topics'))
+            
+    context = {'form': form}
+    return render(request, 'mybl/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+            
+    context = {'topic': topic, 'form': form}
+    return render(request, 'mybl/new_entry.html', context)
+    
+@login_required
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+    
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+            
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'mybl/edit_entry.html', context)
