8c40f9e497685745ca6af9ae240189bf3705a892
ksn38 <ksn38@yandex.ru>
2020-11-05 09:31:11 +0300

    -

--- mybl/views.py
+++ mybl/views.py
@@ -5,19 +5,40 @@ from django.urls import reverse
 from mybl.forms import BpostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 import requests
+from datetime import date
+from datetime import timedelta
+from collections import OrderedDict
 
 
 def index(request):
-    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    currency = response.content
-    currency = currency.decode("cp1251").split('>')
-    dict_curr = {}
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    today = date.today() - timedelta(days=10)
+    dif = today.strftime("?date_req=%d/%m/%Y")
 
-    for i in range(len(currency)):
-        if currency[i] == '<Name':
-            dict_curr[currency[i + 1].split('<')[0]] = float(currency[i + 3].split('<')[0].replace(',', '.'))
-            
-    context = {'dict_curr': dict_curr}
+    def parser(url):
+        response = requests.get(url)
+        currency = response.content.decode("cp1251").split('>')
+        dict_curr = {}
+
+        for i in range(len(currency)):
+            if currency[i] == '<CharCode':
+                dict_curr[currency[i + 1].split('<')[0]] = float(currency[i + 7].split('<')[0].replace(',', '.'))
+
+        return dict_curr
+
+    now = parser(url)
+    delta = parser(url + dif)
+    order_dif = {}
+
+    for key in now.keys():
+      try:
+          order_dif[key] = round((now[key]/delta[key] - 1) * 100, 2)
+      except KeyError:
+          pass      
+      
+    order_dif = OrderedDict(sorted(order_dif.items(), key=lambda item: item[1]))
+    
+    context = {'order_dif': order_dif}
     return render(request, 'mybl/index.html', context)
 
 def blog(request):
