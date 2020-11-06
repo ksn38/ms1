731e4e9e3694f65eeaf03c6f8b63c200c952c370
ksn38 <ksn38@yandex.ru>
2020-11-06 12:40:52 +0300

    -

--- mybl/views.py
+++ mybl/views.py
@@ -26,8 +26,7 @@ def index(request):
         return dict_curr
 
     now = parser(0)
-    
-    delta = 0
+    delta = 2
     
     if(request.GET.get('mybtn')):
         delta = (int(request.GET.get('mytextbox')))
