8c9c607a750138e341c8fc309f8599b47d49741a
ksn38 <ksn38@yandex.ru>
2020-12-09 18:05:49 +0300

    -

--- mybl/static/mybl/script_color.js
+++ mybl/static/mybl/script_color.js
@@ -1,10 +1,20 @@
 let tr = document.querySelectorAll('.color');
+let tri = document.querySelectorAll('.color_inv');
 
 for (let i of tr) {
   if (parseInt(i.textContent) < 0) {
     i.classList.add('bg-danger')
   }
   else if (parseInt(i.textContent) > 0) {
+    i.classList.add('bg-success')
+  }
+}
+
+for (let i of tri) {
+  if (parseInt(i.textContent) > 0) {
+    i.classList.add('bg-danger')
+  }
+  else if (parseInt(i.textContent) < 0) {
     i.classList.add('bg-success')
   }
 }
