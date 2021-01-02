84b18e482aef22d2b7e8b713a5e16aff1680f8a3
ksn38 <ksn38@yandex.ru>
2021-01-02 11:23:18 +0300

    -

--- mybl/static/mybl/tickers.js
+++ mybl/static/mybl/tickers.js
@@ -9,9 +9,40 @@ let chart2 = document.getElementById("line-chart2");
 let chart3 = document.getElementById("line-chart3");
 let onload = true;
 let lengthRD = received_data.length;
+let rcsv = [0, 0, 0, 0];
+let rcst = [0, 0, 0, 0];
+let rctv = [0, 0, 0, 0];
 
 
-let lineChart = function(x, y, xLabel, yLabel, xColor, yColor, chart) {
+let cor = (list1, list2) => {
+  let average = (list) => {
+    return list.reduce((accum, curr) => accum + curr) / list.length;
+  };
+
+  let avgList1 = average(list1);
+  let avgList2 = average(list2);
+
+  let cov = (list1, avgList1, list2, avgList2) => {
+    let list = [];
+    for (let i = 0; i < list1.length; i++) {
+      list[i] = (list1[i] - avgList1)*(list2[i] - avgList2);
+    };
+    return list;
+  };
+
+  let sum = (list) => {
+    return list.reduce((accum, curr) => accum + curr);
+  }
+
+  let dif2 = (list, avg) => {
+    let initialValue = 0;
+    return list.reduce((accum, curr) => accum + ((curr - avg)**2), initialValue);
+  }
+
+  return (sum(cov(list1, avgList1, list2, avgList2)))/Math.sqrt(dif2(list1, avgList1)*dif2(list2, avgList2));
+};
+
+let lineChart = function(x, y, corr, xLabel, yLabel, corrLabel, xColor, yColor, chart) {
   new Chart(chart, {
     type: 'line',
     data: {
@@ -28,6 +59,13 @@ let lineChart = function(x, y, xLabel, yLabel, xColor, yColor, chart) {
           fill: false,
           label: yLabel,
           yAxisID: yLabel,
+        }, { 
+          data: corr,
+          borderColor: '#777777',
+          fill: true,
+          label: corrLabel,
+          yAxisID: corrLabel,
+          pointRadius: 0,
         }
       ]
     },
@@ -50,6 +88,14 @@ let lineChart = function(x, y, xLabel, yLabel, xColor, yColor, chart) {
           type: 'linear',
           position: 'right',
           
+        }, {
+          id: corrLabel,
+          type: 'linear',
+          position: 'right',
+          ticks : {
+            max : 1,    
+            min : -1
+          }
         }]
       }
     }
@@ -64,29 +110,62 @@ for (let i = lengthRD - 50; i < lengthRD; i++) {
   gspc.push(received_data[i]['fields']['gspc']);
 }
 
-lineChart(vix, gspc, 'VIX', 'S&P500', '#ff0000', "#0000ff", chart1);
-lineChart(tnx, gspc, 'TR10', 'S&P500', '#c000ff', "#0000ff", chart2);
-lineChart(vix, tnx, 'VIX', 'TR10', '#ff0000', "#c000ff", chart3);
+console.log(cor(vix, gspc));
+
+for (let i = 0; i <= vix.length - 5; i++) {
+  rcsv.push(cor(gspc.slice(i, i+ 5), vix.slice(i, i+ 5)));
+};
+
+for (let i = 0; i <= vix.length - 5; i++) {
+  rcst.push(cor(gspc.slice(i, i+ 5), tnx.slice(i, i+ 5)));
+};
+
+for (let i = 0; i <= vix.length - 5; i++) {
+  rctv.push(cor(tnx.slice(i, i+ 5), vix.slice(i, i+ 5)));
+};
+
+lineChart(vix, gspc, rcsv, 'VIX', 'S&P500', 'Rolling correlation', '#ff0000', "#0000ff", chart1);
+lineChart(tnx, gspc, rcst, 'TR10', 'S&P500', 'Rolling correlation', '#c000ff', "#0000ff", chart2);
+lineChart(vix, tnx, rctv, 'VIX', 'TR10', 'Rolling correlation', '#ff0000', "#c000ff", chart3);
 
 
 for(let i = 0; i < radio.length; i++){
   radio[i].addEventListener("change", function(){
     item = radio[i].value;
     console.log(item);
+    console.log(item);
     date = [];
     vix = [];
     tnx = [];
     gspc = [];
+    let rcsv = [0, 0, 0, 0];
+    let rcst = [0, 0, 0, 0];
+    let rctv = [0, 0, 0, 0];
     for (let i = lengthRD - item; i < lengthRD; i++) {
       date.push(received_data[i]['fields']['date_added']);
       vix.push(received_data[i]['fields']['vix']);
       tnx.push(received_data[i]['fields']['tnx']);
       gspc.push(received_data[i]['fields']['gspc']);
     }
+    
+    /*while (rollcorr.length < 5 - 1) {
+      rollcorr.push(0);
+    };*/
+    for (let i = 0; i <= item - 5; i++) {
+      rcsv.push(cor(gspc.slice(i, i+ 5), vix.slice(i, i+ 5)));
+    };
+    
+    for (let i = 0; i <= item - 5; i++) {
+      rcst.push(cor(gspc.slice(i, i+ 5), tnx.slice(i, i+ 5)));
+    };
+    
+    for (let i = 0; i <= item - 5; i++) {
+      rctv.push(cor(tnx.slice(i, i+ 5), vix.slice(i, i+ 5)));
+    };
 
-    lineChart(vix, gspc, 'VIX', 'S&P500', '#ff0000', "#0000ff", chart1);
-    lineChart(tnx, gspc, 'TR10', 'S&P500', '#c000ff', "#0000ff", chart2);
-    lineChart(vix, tnx, 'VIX', 'TR10', '#ff0000', "#c000ff", chart3);
+    lineChart(vix, gspc, rcsv, 'VIX', 'S&P500', 'Rolling correlation', '#ff0000', "#0000ff", chart1);
+    lineChart(tnx, gspc, rcst, 'TR10', 'S&P500', 'Rolling correlation', '#c000ff', "#0000ff", chart2);
+    lineChart(vix, tnx, rctv, 'VIX', 'TR10', 'Rolling correlation', '#ff0000', "#c000ff", chart3);
     item = 50;
     date = [];
     vix = [];
@@ -116,5 +195,6 @@ for (let i of tri) {
     i.classList.add('bg-success')
   }else {i.classList.add('bg-secondary')}
 }
+
 
 
