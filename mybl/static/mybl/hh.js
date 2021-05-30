3dd89f57d033614bc7cf5e17f24a15b0cd6833c1
ksn38 <ksn38@yandex.ru>
2021-05-30 14:36:23 +0300

    -

--- mybl/static/mybl/hh.js
+++ mybl/static/mybl/hh.js
@@ -23,12 +23,29 @@ let colorCol = (arr) => {
   }
 }
 
+let colorColInv = (arr) => {
+  let arrInt = [];
+  for (let i = 0; i < arr.length; i++) {
+    arrInt.push(parseInt(arr[i].textContent));
+  }
+  let maxArr = Math.max.apply(null, arrInt);
+  let minArr = Math.min.apply(null, arrInt);
+  for (let i = 0; i < arrInt.length; i++) {
+    if (arrInt[i] > 0) {
+      arr[i].style.backgroundColor = 'rgba(220, 53, 69,'  + arrInt[i]/maxArr + ')';
+    }
+    else if (arrInt[i] < 0) {
+      arr[i].style.backgroundColor = 'rgba(40, 167, 69,'  + arrInt[i]/minArr + ')';
+    }
+  }
+}
+
 colorCol(valChng2020);
 colorCol(val_noexpChng2020);
-colorCol(res_vacChng2020);
+colorColInv(res_vacChng2020);
 colorCol(valChng2021);
 colorCol(val_noexpChng2021);
-colorCol(res_vacChng2021);
+colorColInv(res_vacChng2021);
 
 
 let valNowNod = document.querySelectorAll('.val-now'); 
