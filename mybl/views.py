7f2e8744204bee7b10ea23d245e6aea728c8f201
ksn38 <ksn38@yandex.ru>
2021-10-30 08:01:24 +0000

    delete commented code

--- mybl/views.py
+++ mybl/views.py
@@ -152,75 +152,6 @@ def edit_bpost(request, bpost_id):
     return render(request, 'mybl/edit_bpost.html', context)
     
 def hh(request):
-    '''def apivac(expir):
-        vac = {}
-
-        for i in ['Python', 'C%23', 'c%2B%2B', 'Java', 'Javascript', 'php', 'Ruby', 'Golang', '1c', 'Data scientist', 'Scala', 'iOS', 'Frontend', 'DevOps', 'ABAP', 'Android']:
-            url = 'https://api.hh.ru/vacancies?&' + expir + 'search_field=name&text=' + i
-            response = requests.get(url)
-            val = json.loads(response.content.decode("utf-8"))
-            vac[i] = val['found']
-
-        return vac
-
-
-    def parservac():
-        res = {}
-
-        for i in ['Python', 'C%23', 'c%2B%2B', 'Java', 'Javascript', 'php', 'Ruby', 'Golang', '1c', 'Data scientist', 'Scala', 'iOS', 'Frontend', 'DevOps', 'ABAP', 'Android']:
-            url = 'https://hh.ru/search/resume?clusters=true&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=position&text=' + i
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
-            response = requests.get(url, headers=headers).text
-            parsed_html = bs(response, 'lxml')
-            bloko = parsed_html.find('h1', {'class': 'bloko-header-1'}).text.split(' ')[-1].split('\xa0')
-            if len(bloko) == 3:
-                bloko = ''.join(map(str, bloko[:2]))
-            else:
-                bloko = ''.join(map(str, bloko[:1]))
-            res[i] = int(bloko)
-
-        return res
-
-    date_today = date.today().strftime("%Y-%m-%d")
-    langs = Lang.objects.filter(Q(date_added = date_today))
-
-    if len(langs) == 0:
-        noexp = 'experience=noExperience&'
-        vacs = apivac('')
-        vacs_noexp = apivac(noexp)
-        res = parservac()
-
-        for k, k2 in zip(vacs.keys(), res.keys()):
-            res[k2] = round(res[k2] / vacs[k], 1)
-            vacs_noexp[k] = round(vacs_noexp[k] * 100 / vacs[k])
-
-        for k, v, vne, vrv in zip(vacs.keys(), vacs.values(), vacs_noexp.values(), res.values()):
-            if k == 'c%2B%2B':
-                k = 'cpp'
-            new_values = {'name': k,
-             'val': v, 'val_noexp': vne, 'res_vac': vrv}
-            obj = Lang(**new_values)
-            obj.save()
-        
-        langs = Lang.objects.raw(langs_today)
-        context = {'langs': langs}
-    else:
-        langs = Lang.objects.raw(langs_today)
-        context = {'langs': langs}'''
-    
-    '''context = {}
-    
-    context['langs'] = Lang.objects.raw(langs_today)
-    context['charts'] = Lang.objects.raw(chart_langs)
-    context['charts_march'] = Lang.objects.raw(chart_langs_march)
-    
-    graphs = Lang_graphs.objects.raw("""select id, name, res_vac, date_added from mybl_lang ml where name = 'Python' or name = 'Java' or name = 'Javascript' or name = 'php' or name = 'cpp' order by date_added, name""")
-    context['graphs'] = serializers.serialize('json', graphs)
-
-    graphs_avg = Lang_avg.objects.raw("""select distinct max(id) over(partition by date_added) as id, date_added, avg(val_noexp) over(partition by date_added) as avg_vn, avg(res_vac) over(partition by date_added) as avg_rv from mybl_lang order by date_added""")
-    context['graphs_avg'] = serializers.serialize('json', graphs_avg)'''
-    
     langs = cache.get('langs')
     charts = cache.get('charts')
     charts_march = cache.get('charts_march')
@@ -239,10 +170,6 @@ def about(request):
 def index(request):
     chart_tickers = cache.get('chart_tickers_view')
     tickers5000 = cache.get('tickers5000')
-    
-    #chart_tickers_raw = Ticker.objects.raw(chart_tickers)#"select * from chart_tickers")
-    #tickers5000_raw = Ticker.objects.raw("select * from mybl_ticker mt where id > (select max(id) from mybl_ticker mt2) - 5000")
-    #tickers5000 = serializers.serialize('json', tickers5000_raw)        
         
     context = {'chart_tickers': chart_tickers, 'tickers5000': tickers5000}
             
